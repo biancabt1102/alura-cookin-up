78627eee48a70906d8dadfb6f220d3dc58b50353
Bianca Teixeira <biancabt1102@gmail.com>
2024-01-08 15:20:17 -0300

    feat: Add CardCategoria, Tag and imagens

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,17 +1,19 @@
 <script lang="ts">
   import {obterCategorias} from '@/http/index';
   import type ICategoria from '@/interfaces/ICategoria';
+  import CardCategoria from './CardCategoria.vue';
 
   export default {
-    data(){
-      return {
-        categorias: [] as ICategoria[]
-      }
+    data() {
+        return {
+            categorias: [] as ICategoria[]
+        };
     },
     async created() {
-      this.categorias = await obterCategorias();
-    }
-  }
+        this.categorias = await obterCategorias();
+    },
+    components: { CardCategoria }
+}
 </script>
 
 <template>
@@ -26,7 +28,7 @@
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        {{ categoria.nome }}
+        <CardCategoria :categoria="categoria" />
       </li>
     </ul>
 
